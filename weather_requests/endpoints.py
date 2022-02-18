64306dd0ac63e13603f8b99e1635d79c7bfdf9a2
Podkosa <artem_podkolzin@inbox.ru>
2022-02-18 22:30:55 +0300

    minor

--- weather_requests/endpoints.py
+++ weather_requests/endpoints.py
@@ -2,8 +2,7 @@ from datetime import datetime
 import asyncio
 import typing
 
-from fastapi import Request, Depends
-from fastapi import APIRouter, HTTPException
+from fastapi import Request, Depends, APIRouter, HTTPException, BackgroundTasks
 from sqlalchemy.orm import Session
 
 from database import crud
@@ -15,16 +14,16 @@ router = APIRouter()
 
 #As per test conditions, returns only the temperature and response time. If needed, can be modified to return more info.
 @router.get("/weather", response_model=schemas.WeatherResponse)
-async def weather(request: Request, db: Session = Depends(get_db)):
+async def weather(request: Request, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
     request_time = datetime.utcnow()
     weather_request = schemas.WeatherRequestBase(date=request_time, headers=request.headers)
     weather_request = crud.create_weather_request(db=db, weather_request=weather_request)
     
     try:
         temperature = await asyncio.wait_for(get_weather(), timeout=2)
-        if temperature is not None:
+        if temperature is not None and type(temperature) == float:
             weather_request.temperature = temperature
-            weather_request = crud.update_weather_request(db=db, weather_request=weather_request)
+            background_tasks.add_task(crud.update_weather_request, db=db, weather_request=weather_request)
             return {'temperature':temperature, 'response_time':datetime.utcnow()}
         raise HTTPException (status_code=200, detail="External weather services are unavailable. Please try again later.")
     except asyncio.TimeoutError:
